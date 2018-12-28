015ef325f25be116ef5cf1376114652f8967123a
Madura De Silva <Madura.D@CreativeSoftware.com>
2018-12-28 18:05:52 +0530

    firebase connected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initializeApp, database} from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'final-project';
+
+  constructor() {
+      // Initialize Firebase
+      var config = {
+        apiKey: "AIzaSyDHTdmX0aWNJwuISWyGmt32Cxsu10HFZuA",
+        authDomain: "final-project-recording-7e32c.firebaseapp.com",
+        databaseURL: "https://final-project-recording-7e32c.firebaseio.com",
+        projectId: "final-project-recording-7e32c",
+        storageBucket: "final-project-recording-7e32c.appspot.com",
+        messagingSenderId: "122855990924"
+      };
+      initializeApp(config);
+      
+      var root = database().ref();
+      root.on('value',function(snap) {
+        console.log(snap.val());
+      });
+  }
 }
